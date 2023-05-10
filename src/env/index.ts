a0da3e2f833770213e8b5a2a448de5f6196d3524
Vinicius <vinicius@inovahu.com>
2023-05-09 21:21:46 -0300

    fix variable

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,7 +5,7 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
-  MIGRATIONS_DIR: z.string(),
+  // MIGRATIONS_DIR: z.string(),
   PORT: z.coerce.number().default(3333),
   REGION: z.string(),
   DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
